56444332b8deafecc7fd3aa60130bfe866961723
Alberto Godoy <76492268+albertogodoy@users.noreply.github.com>
2025-03-25 00:31:52 -0400

    Eliminar dist y node_modules del control de versiones

--- dist/core/Column.d.ts
+++
@@ -1,9 +0,0 @@
-import { IColumn } from "../interfaces/IColumn";
-export declare class Column implements IColumn {
-    name: string;
-    type: 'string' | 'number' | 'boolean' | 'object' | 'array' | 'date';
-    unique?: boolean;
-    required?: boolean;
-    defaultValue?: any;
-    constructor(config: IColumn);
-}
--- dist/interfaces/IColumn.d.ts
+++
@@ -1,7 +0,0 @@
-export interface IColumn {
-    name: string;
-    type: 'string' | 'number' | 'boolean' | 'object' | 'array' | 'date';
-    unique?: boolean;
-    required?: boolean;
-    defaultValue?: any;
-}
